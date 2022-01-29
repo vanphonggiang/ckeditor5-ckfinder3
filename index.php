19660fa4bd4e4d4d75bdd8cd99bf0ecbc9cc3124
GiangWeb <vanphonggiang@gmail.com>
2022-01-29 21:16:22 +0700

    Fix head

--- index.php
+++ index.php
@@ -5,10 +5,9 @@
     <title>CKEditor 5 Integration Example</title>
     <script src="ckeditor/ckeditor.js"></script>
     <script src="ckfinder/ckfinder.js"></script>
- </head>
- <body>
+</head>
+<body>
     <textarea class="ckeditor" id="editor"></textarea>
-    
     <script type="text/javascript">
         ClassicEditor
             .create( document.querySelector( '#editor' ), {
@@ -18,7 +17,7 @@
                 toolbar: [ 'ckfinder', 'imageUpload', '|', 'heading', '|', 'bold', 'italic', '|', 'undo', 'redo' ]
             }).catch( function( error ) {
                 console.error( error );
-            });
-     </script>
- </body>
- </html>
+        });
+    </script>
+</body>
+</html>
